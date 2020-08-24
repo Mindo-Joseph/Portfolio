6ea78c4aab8798b65e6d1d9159c08e87d1d38d7f
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-08-25 02:51:47 +0300

    style: Style the contact form

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%"
 }
+
+var accordion = document.getElementByClassName("accordion");
+var i;
+for (i=0; i<accordion.length; i++){
+  accordion[i].addEventListener("click",function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  }
+  
+  );
+}
